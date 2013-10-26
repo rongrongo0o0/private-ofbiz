6ba16c1c802702384e51017e5a9fb27c8bf074af
rongrongo0o0 <ryangxx@gmail.com>
2013-10-26 09:13:35 +0800

    Update HelloWorld.java

--- src/com/hg/test/HelloWorld.java
+++ src/com/hg/test/HelloWorld.java
@@ -3,7 +3,7 @@ package com.hg.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world ");
-		//add from web
+		//add from web 2
 		System.out.println("test");
 	}
 }
